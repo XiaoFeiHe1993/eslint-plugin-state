6d32462824ebc4648c6c5d61e27e2b1699b1b985
JackFei <1712776213@qq.com>
2019-06-18 00:03:28 +0800

    feat: 增加vue

---
+++ test.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>vue</div>
+</template>
+
+<script>
+  export default {
+    name: "test",
+    data() {
+      return {
+        name: 'name'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ test/lib/rules/dataVue.js
@@ -0,0 +1,51 @@
+/**
+ * @author xiaofeihe
+ */
+'use strict';
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+var rule = require('../../../lib/rules/no-data-comment-line'),
+  RuleTester = require('eslint').RuleTester;
+
+RuleTester.setDefaultConfig({
+  parser: 'babel-eslint'
+});
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+var ruleTester = new RuleTester();
+ruleTester.run('no-state-comment-line', rule, {
+  valid: [
+    `
+     export default {
+      data() {
+        return {
+          name: 'name' // 名字
+        }
+      }
+    }
+    `
+  ],
+  invalid: [
+    {
+      code: `
+        export default {
+          data() {
+            return {
+              name: 'name'
+            }
+          }
+        }
+      `,
+      errors: [{
+        message: 'please add comments for data variate',
+        type: 'Property'
+      }]
+    }
+  ]
+});
